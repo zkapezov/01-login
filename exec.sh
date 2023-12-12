6b983fd20dce3f3271e070ef87caabe21234afef
Zhandos Kapezov <zhandos.kapezov@gmail.com>
2023-12-12 23:36:10 +0600

    update ports

--- exec.sh
+++ exec.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t auth0-django-01-login .
-docker run --env-file .env -p 8081:8081 -it auth0-django-01-login
+docker run --env-file .env -p 8080:8080 -it auth0-django-01-login
