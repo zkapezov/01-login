ec3225bc4b3ddfb88fb75e4e867e2fbc74a366ef
Zhandos Kapezov <zhandos.kapezov@gmail.com>
2023-12-13 22:09:07 +0600

    add webhook url

--- webappexample/urls.py
+++ webappexample/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("logout", views.logout, name="logout"),
     path("callback", views.callback, name="callback"),
     path("userhub/callback", views.userhub_callback, name="userhub_callback"),
+    path("userhub/webhook", views.userhub_webhook, name="userhub_webhook"),
 ]
