2c65f45f477a4a01445f124b2098e174586da06f
Zhandos Kapezov <zhandos.kapezov@gmail.com>
2023-12-13 23:03:48 +0600

    update logger severity

--- webappexample/settings.py
+++ webappexample/settings.py
@@ -120,15 +120,15 @@ LOGGING = {
     "disable_existing_loggers": False,
     "handlers": {
         "file": {
-            "level": "DEBUG",
+            "level": "INFO",
             "class": "logging.FileHandler",
-            "filename": "/home/app/debug.log",
+            "filename": "/home/app/info.log",
         },
     },
     "loggers": {
         "django": {
             "handlers": ["file"],
-            "level": "DEBUG",
+            "level": "INFO",
             "propagate": True,
         },
     },
